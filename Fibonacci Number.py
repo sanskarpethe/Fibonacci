431c7e0c900eb0057bc9c47f2f3d5986e209bf3d
sanskarpethe <87526905+sanskarpethe@users.noreply.github.com>
2021-07-27 08:57:05 +0530

    Add files via upload

---
+++ Fibonacci Number.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+nterms = int(input("How many terms? "))
+
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+    print("Please enter a positive integer")
+elif nterms == 1:
+    print("Fibonacci sequence upto",nterms,":")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        newterm = n1 + n2
+        n1 = n2
+        n2 = newterm
+        count += 1
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
